dc0b98e45101b2c86c4e9f1a18362a79ff7d95ad
unknown <pedro.henrisz@gmail.com>
2024-03-06 12:07:03 -0300

    Inicializando o projeto

---
+++ src/app/components/chat-response/chat-response.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Message } from '../../types/message.type';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chat-response',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chat-response.component.html',
+  styleUrl: './chat-response.component.scss'
+})
+export class ChatResponseComponent {
+  @Input() message!: Message;
+
+}
---
+++ src/app/pages/chat/chat.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { MuseumComponent } from '../../icons/museum/museum.component';
+import { ArrowLeftComponent } from '../../icons/arrow-left/arrow-left.component';
+import { ChatSuggestionsComponent } from '../../components/chat-suggestions/chat-suggestions.component';
+import { CommonModule } from '@angular/common';
+import { Message } from '../../types/message.type';
+import { ChatDialogComponent } from '../../components/chat-dialog/chat-dialog.component';
+import { MessageService } from '../../services/message.service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-chat',
+  standalone: true,
+  imports: [MuseumComponent, 
+    ArrowLeftComponent, 
+    ChatSuggestionsComponent, 
+    CommonModule, 
+    ChatDialogComponent],
+    providers: [MessageService],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.scss'
+})
+export class ChatComponent {
+  messages: Message [] = JSON.parse(localStorage.getItem("messages") ?? "[]")
+
+  constructor(private service: MessageService) { }
+
+  updatelocalStorage(){
+    localStorage.setItem("messages", JSON.stringify(this.messages))
+  }
+
+  sendSuggestedQuestion(question: string){
+    this.messages.push({
+      type: 'request',
+      message: question
+    })
+
+    this.updatelocalStorage()
+    this.sendMessage(question)
+  }
+
+  sendMessage(message: string){
+    this.service.send(message).subscribe({
+      next: (body) => {
+        this.messages.push({
+          type: "response",
+          message:body.response
+        })
+        this.updatelocalStorage()
+      }
+    })
+  }
+}
